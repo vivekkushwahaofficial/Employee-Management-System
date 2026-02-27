86759646612e5201ae31b3788e6a0b1e5c5f15b4
vivekkushwahaofficial <185205704+vivekkushwahaofficial@users.noreply.github.com>
2026-02-27 15:36:26 +0530

    Configure application to use environment variables for database connection and update dependencies

--- ems-backend/src/main/java/net/javaguides/ems/EmsBackendApplication.java
+++ ems-backend/src/main/java/net/javaguides/ems/EmsBackendApplication.java
@@ -1,5 +1,5 @@
 package net.javaguides.ems;
-
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmsBackendApplication {
 
 	public static void main(String[] args) {
+    Dotenv dotenv = Dotenv.load();
+    System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+    System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+    System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(EmsBackendApplication.class, args);
 	}
 
