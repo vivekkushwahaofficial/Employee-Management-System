b742e09aac26f974c9e8edb42fcb0e0b6bb6b3b4
vivekkushwahaofficial <185205704+vivekkushwahaofficial@users.noreply.github.com>
2026-02-27 14:25:54 +0530

    Initial commit - Full Stack Employee Management System

---
+++ ems-frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import FooterComponent from './components/FooterComponent'
+import HeaderComponent from './components/HeaderComponent'
+import ListEmployeeComponent from './components/ListEmployeeComponent'
+import EmployeeComponent from './components/EmployeeComponent'
+import{BrowserRouter, Routes, Route} from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+      <HeaderComponent />
+      <Routes>
+        {/* // http://localhost:3000 */}
+        <Route path='/' element={ <ListEmployeeComponent />}></Route>
+          {/* // http://localhost:3000/employees */}
+          <Route path='/employees' element={ <ListEmployeeComponent />}></Route>
+
+          {/* // http://localhost:3000/add-employee */}
+          <Route path='/add-employee' element={ <EmployeeComponent />}></Route>
+      </Routes> 
+      <FooterComponent />
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+      <footer className='footer'>
+        <span>All right reserved by Vivek Kushwaha</span>
+      </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
