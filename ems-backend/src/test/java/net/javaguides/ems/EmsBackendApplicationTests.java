f18f1a1cf90c0bd8be317b12ec0a06bd3c021be8
vivekkushwahaofficial <185205704+vivekkushwahaofficial@users.noreply.github.com>
2026-02-27 16:04:10 +0530

    Add Dockerfile, H2 database dependency, and test properties for integration testing

--- ems-backend/src/test/java/net/javaguides/ems/EmsBackendApplicationTests.java
+++ ems-backend/src/test/java/net/javaguides/ems/EmsBackendApplicationTests.java
@@ -2,8 +2,10 @@ package net.javaguides.ems;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EmsBackendApplicationTests {
 
 	@Test
