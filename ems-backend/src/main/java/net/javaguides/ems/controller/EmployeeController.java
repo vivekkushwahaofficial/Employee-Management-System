f2c22523494041ce99026006ae7a76bb50616faf
vivekkushwahaofficial <185205704+vivekkushwahaofficial@users.noreply.github.com>
2026-05-27 01:41:40 +0530

    updated cross origin

--- ems-backend/src/main/java/net/javaguides/ems/controller/EmployeeController.java
+++ ems-backend/src/main/java/net/javaguides/ems/controller/EmployeeController.java
@@ -1,15 +1,24 @@
 package net.javaguides.ems.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import lombok.AllArgsConstructor;
 import net.javaguides.ems.dto.EmployeeDto;
 import net.javaguides.ems.service.EmployeeService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-@CrossOrigin("*")
+@CrossOrigin("https://employee-management-system-wnla.vercel.app")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/employees")
